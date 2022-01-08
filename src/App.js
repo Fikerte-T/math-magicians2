36734069f7aba4de0139f0bbaa07b03f4e52a677
fikerte <tofikertesfaye@gmail.com>
2022-01-08 07:10:51 +0300

    Make App component class based

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
