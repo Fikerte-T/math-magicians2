2e2d0deab5167b104708499a09c00fa1e21ea3eb
fikerte <tofikertesfaye@gmail.com>
2022-01-14 17:42:51 +0300

    Add routes

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const Quote = () => {
-    <div>
-        <blockquote>Mathematics is not about numbers, equations, computations, or algorithms:
-        it is about understanding. - <span className="author">William Paul Thurson</span>
-        </blockquote>
-    </div>
-}
+const Quote = () => (
+  <div>
+    <blockquote>
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding. -
+      <span className="author">William Paul Thurson</span>
+    </blockquote>
+  </div>
+);
 
 export default Quote;
