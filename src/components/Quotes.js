95d3c3f2bd0a2cf450f1146cd972813e2b0938d8
fikerte <tofikertesfaye@gmail.com>
2022-01-14 15:49:02 +0300

    Add Quotes component

---
+++ src/components/Quotes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Quote = () => {
+    <div>
+        <blockquote>Mathematics is not about numbers, equations, computations, or algorithms:
+        it is about understanding. - <span className="author">William Paul Thurson</span>
+        </blockquote>
+    </div>
+}
+
+export default Quote;
